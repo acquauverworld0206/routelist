6465c6dd3ac53fdac19fe214dedd0b887508636a
acquauverworld0206 <acquauverworld0206@gmail.com>
2025-11-10 21:22:08 +0900

    修正3

--- db/migrate/20251106024512_create_candidate_spots.rb
+++ db/migrate/20251106024512_create_candidate_spots.rb
@@ -5,7 +5,7 @@ class CreateCandidateSpots < ActiveRecord::Migration[7.1]
     create_table :candidate_spots do |t|
       t.references :group, null: false, foreign_key: true
       t.references :spot, null: false, foreign_key: true
-      t.references :added_by_user, null: false, foreign_key: { to_table: :users }
+      t.references :added_by, null: false, foreign_key: { to_table: :users }
       t.text :votes
       t.boolean :is_decided, null: false, default: false
       t.integer :decided_order
